e8825e7b292baf7977f1165478659d78695a41c4
Emilio M. Pomarada <pomaradaemilio92@gmail.com>
2023-08-04 13:11:06 -0300

    calculadoracon props.children, componentes con etiqueta de apertura y cierre con libreria externa

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import Boton from './componentes/Boton';
 import Pantalla from './componentes/Pantalla';
 import BotonClear from './componentes/BotonClear';
 import { useState } from 'react';
+import {evaluate, identity} from 'mathjs';
+
 
 function App() {
   const [input, setInput] = useState('');
   const agregarInput = val => {
     setInput(input + val);
+  };
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    }else {
+      alert("por favor ingrese valores para realizar los calculos.");
+    }
+    
   };
   return (
     <div className="App">
@@ -39,13 +49,15 @@ function App() {
             <Boton manejarClic={agregarInput}>*</Boton>
          </div>
          <div className='fila'>
-            <Boton manejarClic={agregarInput}>=</Boton>
+            <Boton manejarClic={calcularResultado}>=</Boton>
             <Boton manejarClic={agregarInput}>0</Boton>
             <Boton manejarClic={agregarInput}>.</Boton>
             <Boton manejarClic={agregarInput}>/</Boton>
          </div>
          <div className='fila'>
-            <BotonClear>Clear</BotonClear>
+            <BotonClear manejarClear={() => setInput('')}>
+              Clear
+            </BotonClear>
          </div>
       </div>
     </div>
